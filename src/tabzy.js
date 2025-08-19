9975e3a2cf636ac018a925a262fed655b4928d17
Your Name <your-email@example.com>
2025-08-19 10:49:58 +0700

    Xây dựng logic khởi tạo cho Tabzy

---
+++ src/tabzy.js
@@ -0,0 +1,63 @@
+function Tabzy(selector) {
+    this.container = document.querySelector(selector);
+    if (!this.container) {
+        console.error(`Tabzy: No container found selector ${selector}`);
+        return;
+    }
+
+    this.tabs = Array.from(this.container.querySelectorAll("li a"));
+    if (!this.tabs.length) {
+        console.error(`Tabzy: No tabs found inside the container`);
+        return;
+    }
+
+    this.panels = this.tabs
+        .map((tab) => {
+            const panel = document.querySelector(tab.getAttribute("href"));
+            if (!panel) {
+                hasError = true;
+                console.error(
+                    `Tabzy: No panel found for selector '${tab.getAttribute(
+                        "href"
+                    )}'`
+                );
+            }
+            return panel;
+        })
+        .filter(Boolean);
+
+    if (this.tabs.length !== this.panels.length) return;
+
+    this._init();
+}
+
+Tabzy.prototype._init = function () {
+    const tabActive = this.tabs[0];
+    tabActive.closest("li").classList.add("tabzy--active");
+
+    this.panels.forEach((panel) => {
+        panel.hidden = true;
+    });
+
+    this.tabs.map((tab) => {
+        tab.onclick = (e) => this._handleTabClick(e, tab);
+    });
+
+    const panelActive = this.panels[0];
+    panelActive.hidden = false;
+};
+
+Tabzy.prototype._handleTabClick = function (e, tab) {
+    e.preventDefault();
+
+    this.tabs.forEach((tab) => {
+        tab.closest("li").classList.remove("tabzy--active");
+    });
+
+    tab.closest("li").classList.add("tabzy--active");
+    this.panels.forEach((panel) => {
+        panel.hidden = true;
+    });
+    const panelActive = document.querySelector(tab.getAttribute("href"));
+    panelActive.hidden = false;
+};
