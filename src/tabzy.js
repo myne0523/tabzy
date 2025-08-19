8fbe6eb2fa37ac2bfec2c14fb3cccc741af577b1
Your Name <your-email@example.com>
2025-08-19 20:28:48 +0700

    Xử lý ghi nhớ đồng thời nhiều tab

--- src/tabzy.js
+++ src/tabzy.js
@@ -35,6 +35,8 @@ function Tabzy(selector, options) {
         options
     );
 
+    this.paramKey = selector.replace(/[^a-zA-Z0-9]/g, "");
+
     this._originalHtml = this.container.innerHTML;
 
     this._init();
@@ -42,12 +44,14 @@ function Tabzy(selector, options) {
 
 Tabzy.prototype._init = function () {
     const params = new URLSearchParams(location.search);
-    const tabSelector = params.get("tab");
+    const tabSelector = params.get(this.paramKey);
     const tab =
         (this.opt.remember &&
             tabSelector &&
             this.tabs.find(
-                (tab) => tab.getAttribute("href") === tabSelector
+                (tab) =>
+                    tab.getAttribute("href").replace(/[^a-zA-Z0-9]/g, "") ===
+                    tabSelector
             )) ||
         this.tabs[0];
 
@@ -77,11 +81,12 @@ Tabzy.prototype._activateTab = function (tab) {
     panelActive.hidden = false;
 
     if (this.opt.remember) {
-        history.replaceState(
-            null,
-            null,
-            `?tab=${encodeURIComponent(tab.getAttribute("href"))}`
-        );
+        const params = new URLSearchParams(location.search);
+        const paramValue = tab
+            .getAttribute("href")
+            .replace(/[^a-zA-Z0-9]/g, "");
+        params.set(this.paramKey, paramValue);
+        history.replaceState(null, null, `?${params}`);
     }
 };
 
